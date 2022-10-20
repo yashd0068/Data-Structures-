15f886626ea0a064a3938e674871eb484bcc2d86
Yash Dubey <113628880+yashd0068@users.noreply.github.com>
2022-10-20 23:20:42 +0530

    Create Fitness.c

---
+++ Fitness.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+int tc;
+int i;
+scanf("%d\n",&tc);
+for(i=0;i<tc;i++){
+    int X;
+    int to;
+    scanf("%d\n",&X);
+    to=X*2*5;
+    printf("%d\n",to);
+}
+	return 0;
+}
